590084d746ecc9cdf9da9e847d5c2a24c1fcb78b
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-08-24 12:53:11 +0100

    added robot new and create

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,32 @@
+class RobotsController < ApplicationController
+  # before_action :find_user, only: [ :new, :create ]
+
+  def new
+    # need to find the user the robot belongs to
+    # via the url params :user_id
+    @robot = Robot.new
+  end
+
+  def create
+    @robot = Robot.new(robot_params)
+    # to specifiy the user the robot belongs to we need to set .user = to @user
+    @robot.user = current_user
+    if @robot.save
+      # return to the user the robot belongs to
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # def find_user
+  #   @user = User.find(params[:user_id])
+  # end
+
+  def robot_params
+    # strong params for robots
+    params.require(:robot).permit(:name, :category, :description, :price_per_day)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'robots/new'
+  get 'robots/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :robots, only: [ :index, :show, :new, :create ]
 end
