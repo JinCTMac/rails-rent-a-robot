fb6066194bf417750e8d42f1a8d7d5fc5815a952
stephanie ezimoha <stephanie.ezimoha@yahoo.co.uk>
2021-08-27 14:10:59 +0100

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,22 +10,41 @@ puts "Cleaning up database..."
 
 Booking.destroy_all
 Robot.destroy_all
+User.destroy_all
 
 puts "Database cleaned"
 
-User.create(email: "asdf@gmail.com", password: "123456")
+def attach_photo(user)
+  url = "https://source.unsplash.com/random?sig=#{rand(1..60)}/&avatar/300x300"
+  file = URI.open(url)
+  user.photo.attach(io: file, filename: "#{user.first_name.gsub(" ", "-")}.jpeg", content_type: 'image/jpeg')
+end
+
+user_1 = User.create(email: "mary@gmail.com", password: "123456", first_name: "Mary", last_name: "Smith" )
+attach_photo(user_1)
+
+user_2 = User.create(email: "john@gmail.com", password: "123456", first_name: "John", last_name: "Smith" )
+attach_photo(user_2)
+
+user_3 = User.create(email: "andy@gmail.com", password: "123456", first_name: "Andy", last_name: "Smith" )
+attach_photo(user_3)
+
+
+USER_ARRAY = [user_1, user_2, user_3]
 ROBOT_CATEGORIES = ['Gardening', 'Chef', 'Driver', 'Home', 'Nanny']
+
 10.times do
   robot = Robot.create!(
     name: Faker::Name.first_name,
     category: ROBOT_CATEGORIES.sample,
     description: Faker::Quote.famous_last_words,
     price_per_day: rand(10..100),
-    user: User.first
+    user: USER_ARRAY.sample
   )
   url = "https://source.unsplash.com/random?sig=#{rand(1..60)}/&robot/800x600"
   file = URI.open(url)
   robot.photo.attach(io: file, filename: "#{robot.name.gsub(" ", "-")}.jpeg", content_type: 'image/jpeg')
 end
+
 
 puts "#{Robot.count} Robots created"
